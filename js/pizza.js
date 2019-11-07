3871741506756e70e36cdfc484df55f683930c95
DjCooGie <evansonchagwa01@gmail.com>
2019-11-08 01:55:43 +0300

    added images to the media folder and a fonts.css file

---
+++ js/pizza.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+$(document).ready(function () {
+
+// Blinking paragraph
+  setInterval(function () {
+    $(".blink").fadeOut(function () {
+      $(this).fadeIn();
+    });
+  }, 1200)
+
+ 
+
+
+  
+
+});
